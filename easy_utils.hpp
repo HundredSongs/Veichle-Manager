a0a07d3c99eb2ce9a76a641ed023ed9707f15a3c
unknown <joaop.s.c.roque@gmail.com>
2024-03-05 15:52:43 +0000

    print Function

--- easy_utils.hpp
+++ easy_utils.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <string>
 
-// 1ª Função
+// 1ª Função ------------------------------------
 std::string input(const std::string& msg = ""){
     
     std::string text;
@@ -11,7 +12,7 @@ std::string input(const std::string& msg = ""){
     return text;
 }
 
-// 2ª Função
+// 2ª Função ------------------------------------
 template<typename T>
 std::string to_string(const T& t){
 
@@ -19,7 +20,7 @@ std::string to_string(const T& t){
 
 }
 
-// 4ª Função
+// 4ª Função ------------------------------------
 struct print_params {
     std::string sep{" "};
     std::string end{"\n"};
@@ -27,6 +28,24 @@ struct print_params {
 
 template<typename Container>
 void print(const Container& cont, const print_params& p = print_params()){
+
+
+    //auto size = sizeof(cont)/sizeof(Container);
+
+    for(int i = 0; i < cont.size(); i++){
+
+        if(i == cont.size() - 1){
+            std::cout << cont[i] << p.end;
+        } 
+        else {
+        std::cout << cont[i] << p.sep;
+        }
+    }
+}
+
+
+template<typename Container>
+void print(const Container& cont, std::ostream& out, const print_params& p = print_params()){
 
     for(int i = 0; i < cont.size(); i++){
 
@@ -37,12 +56,4 @@ void print(const Container& cont, const print_params& p = print_params()){
         std::cout << cont[i] << print_params().sep;
         }
     }
-
-    return ;
-}
-
-template<typename Container>
-void print(const Container& cont, std::ostream& out, const print_params& p = print_params()){
-
-
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <cstring>
+#include <fstream>
 
 #include "easy_utils.hpp"
 
@@ -7,5 +10,12 @@ int main(){
 
     std::vector<int> vals{10, 50, 23};
     print(vals);
-    
+    print(vals, {.sep = "//"});
+    print(vals, {.end = "$$"});
+    print(vals, {.sep = "--" , .end = "FIM\n"});
+    std::string s ("Alberto");
+    print(s);
+    print(s , {.sep = ""});
+    //ofstream file("dados.txt");
+    //print(vals, file, {.sep = ","}); 
 }
