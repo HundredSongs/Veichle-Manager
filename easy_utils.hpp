7460df913e0725f26c3edad68b8ab1902e5f1b4b
unknown <joaop.s.c.roque@gmail.com>
2024-03-05 13:32:05 +0000

    print Function

--- easy_utils.hpp
+++ easy_utils.hpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// 1º Função
+// 1ª Função
 std::string input(const std::string& msg = ""){
     
     std::string text;
@@ -14,6 +14,31 @@ std::string input(const std::string& msg = ""){
 // 2ª Função
 template<typename T>
 std::string to_string(const T& t){
+
+
+
+}
+
+// 4ª Função
+struct print_params {
+    std::string sep{" "};
+    std::string end{"\n"};
+};
+
+template<typename Container>
+void print(const Container& cont, const print_params& p = prints_params()){
+
+    for(int i = 0; i < cont.size(); i++){
+        if( i == cont.size() - 1){
+            std::cout << cont[i] << print_params.end();
+        } else
+        std::cout << cont[i] << print_params.sep();
+    }
+
+}
+
+template<typename Container>
+void print(const Container& cont, std::ostream& out = "", const print_params& p = prints_params()){
 
 
 
