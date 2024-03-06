85bd4dd5d40082b065a1355b4c5ff53280a258b2
unknown <joaop.s.c.roque@gmail.com>
2024-03-06 12:14:44 +0000

    Added ifndef to files

---
+++ src/easy_std/easy_random.hpp
@@ -0,0 +1,8 @@
+#ifndef __EASY_RANDOM_HPP__
+#define __EASY_RANDOM_HPP__
+
+namespace easy_std {
+
+}
+
+#endif
---
+++ src/easy_std/easy_text.hpp
@@ -0,0 +1,8 @@
+#ifndef __EASY_TEXT_HPP__
+#define __EASY_TEXT_HPP__
+
+namespace easy_std {
+
+}
+
+#endif
