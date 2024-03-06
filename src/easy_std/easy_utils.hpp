d25bafd2aec1080b4340f685ac3d1dc49ba119d2
sk_sendo <liciniofeliciano@gmail.com>
2024-03-06 09:07:18 +0000

    qq coisa

--- src/easy_std/easy_utils.hpp
+++ src/easy_std/easy_utils.hpp
@@ -1,48 +1,37 @@
-#include <iostream>
-
-// 1ª Função
-std::string input(const std::string& msg = ""){
-    
-    std::string text;
-
-    std::cout << msg;
-    std::getline(std::cin, text);
-
-    return text;
-}
-
-// 2ª Função
-template<typename T>
-std::string to_string(const T& t){
+// FIRST HEADER-ONLY FILE
 
 
+#ifndef __EASY_UTILS_HPP__
+#define __EASY_UTILS_HPP__
 
-}
 
-// 4ª Função
-struct print_params {
-    std::string sep{" "};
-    std::string end{"\n"};
-};
+namespace easy_std {
 
-template<typename Container>
-void print(const Container& cont, const print_params& p = print_params()){
-
-    for(int i = 0; i < cont.size(); i++){
-
-        if(i == cont.size() - 1){
-            std::cout << cont[i] << print_params().end;
-        } 
-        else {
-        std::cout << cont[i] << print_params().sep;
-        }
+    // 1ª Função
+    inline std::string input(const std::string& msg) {
+        std::cout << msg;
+        std::string str;
+        std::getline(std::cin, str);
+        return str;
     }
 
-    return ;
+    // 2ª Função
+    template <typename T>
+    inline std::string to_string(const T& t) {
+        std::ostringstream ss;
+        ss << t;
+        return ss.str();
+    }
+    // 2ª Função OVERLOADING
+    template <typename T>
+    inline std::string to_string(const std::vector<T>& vec) {
+        std::stringstream ss;
+            for (int i = 0; i < vec.size();i += 1) {
+                ss << vec[i];
+            }
+        return ss.str();
+    }
+
 }
 
-template<typename Container>
-void print(const Container& cont, std::ostream& out, const print_params& p = print_params()){
-
-
-}
+#endif
--- src/easy_std/test_easy_std.cpp
+++ src/easy_std/test_easy_std.cpp
@@ -5,27 +5,43 @@
 
 # include <iostream>
 # include <vector>
+# include <string>
+# include <sstream>
+
+#include <docopt/docopt.h>
+
+# include "easy_utils.hpp"
+
+using namespace easy_std;
 
 
-std::string input(const std::string& msg) {
-    std::cout << msg;
-    std::string str;
-    getline(std::cin, str);
-    return str;
-}
+int main(int argc, char* argv[]) {
 
+    std::string ast(10, '*');
 
-int main() {
+    /////////////////////////////////////////////////////////////////////////
+    std::cout << "\n" << ast << " INPUT " << ast << "\n";
 
-    std::cout << "\n***** INPUT *****\n";
     std::string name1 = input("Como se chama? ");
-    std::cout << "Nome: " << name1 << std::endl;
+    std::cout << "Nome: " << name1 << std::endl; 
+
+    /////////////////////////////////////////////////////////////////////////
+    std::cout << "\n" << ast << " NUM_TO_STRING " << ast << "\n";
+
+    int valor1 = 1597;
+    std::cout << "valor1: " << valor1 << " => " << "to_string(valor1) => " << to_string(valor1) << std::endl;
+
+    double valor2 = 72.89;
+    std::cout << "valor2: " << valor2 << " => " << "to_string(valor2) => " << to_string(valor2) << std::endl;
+    
+    /////////////////////////////////////////////////////////////////////////
+    std::cout << "\n" << ast << " VECTOR_TO_STRING " << ast << "\n";
 
 
-    std::vector<int> vals {10, 72, -500};
-    for (const auto& val : vals) {
-        std::cout << val << std::endl;
-    }  
+
+
+
+
 
     return 0;
 }
